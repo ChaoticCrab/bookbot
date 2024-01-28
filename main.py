9b41abca9583c764a311074e48b49c95115e9947
ChaoticCrab <marikawalz22@gmail.com>
2024-01-28 23:07:02 +0100

    added funcitons to main (word count, letter count, report)

--- main.py
+++ main.py
@@ -1,2 +1,24 @@
 with open("/home/server/workspace/github.com/ChaoticCrab/bookbot/Frankenstein.txt") as f:
     file_contents = f.read()
+
+def word_count(string):
+    word_list = string.split()
+    return len(word_list)
+
+def letter_count(string):
+    letters = {}
+    text = string.lower()
+    for elem in text:
+        if elem not in letters:
+            letters[elem] = 1
+        else:
+            letters[elem] += 1
+    return letters
+
+def print_report(dict):
+    print(dict)
+    for char, count in dict.items():
+        if char.isalpha():
+            print("The " + str(char) + " character was found " + str(count) + " times")
+    
+print_report(letter_count(file_contents))
